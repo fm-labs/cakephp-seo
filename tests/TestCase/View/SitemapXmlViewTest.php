f21b86d5bd8e064d2b0a1e5b0bc60de3cc94e750
fm-labs <flowmotionlabs@gmail.com>
2020-04-12 13:20:02 +0200

    Added test

---
+++ tests/TestCase/View/SitemapXmlViewTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Seo\Test\View;
+
+use Cake\TestSuite\TestCase;
+use Seo\View\SitemapXmlView;
+
+class SitemapXmlViewTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testRender(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
