500a7194c79967dc99f69018e83d1c674252ea53
fm-labs <flowmotionlabs@gmail.com>
2023-03-27 12:13:38 +0200

    Moved google analytics stuff to separate plugin

--- templates/element/Tracking/google_gtag.php
+++
@@ -1,7 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=<?= $trackingId; ?>"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '<?= $trackingId; ?>');
-</script>
